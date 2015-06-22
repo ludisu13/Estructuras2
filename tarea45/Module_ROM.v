3f89c889f17cf6249cdc6156fd989de148bbcc9c
Luis Diego Soto Ugalde <ludisu13@gmail.com>
2015-06-22 02:22:07 -0600

    comandomemoria

--- tarea45/Module_ROM.v
+++ tarea45/Module_ROM.v
@@ -5,21 +5,11 @@ module ROM
 	input  wire[9:0]  		iAddress,
 	output reg [15:0] 		oInstruction
 );	
+reg [15:0] Memory [10:0];
+initial $readmemh("instructions.txt",Memory);
 always @ ( iAddress )
 begin
-	case (iAddress)
-	
-	0: oInstruction = { `NOP ,24'd400   };
-	1: oInstruction = { `STO ,`R1,16'd13};
-	2: oInstruction = { `STO ,`R4,16'd14};
-	3: oInstruction = { `NOP ,24'd400    };	
-	4: oInstruction = { `gIMUL ,`R3, `R4, `R1};
-	5: oInstruction = { `LED ,8'b0, `R3,8'b0 };
-	6: oInstruction = { `JMP ,  8'd1,16'b0   };
-	
-	default:
-		oInstruction = { `LED ,  24'b10101010 };		//NOP 
-	endcase	
+	oInstruction=Memory[iAddress];
 end
 	
 endmodule
