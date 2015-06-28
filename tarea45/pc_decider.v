40834759b60c1e3a79f3cb0bfeb593134beb7124
juan199 <juansanchezbrenes@gmail.com>
2015-06-28 16:42:39 -0600

    pc_decider

--- tarea45/pc_decider.v
+++ tarea45/pc_decider.v
@@ -1,49 +1,3 @@
-module pc_Decider(
-input wire Clock,
-input wire Reset,
-output wire [9:0]wIP,
-input wire wBranchTaken,
-input wire wJumpTaken,
-input wire [9:0] wBranchAddress
-);
-
-wire [9:0]wInitialIP;
-wire [9:0]wDestination;
-assign wInitialIP = (Reset) ? 10'b0 : wDestination;
-assign wIP = (wBranchTaken|wJumpTaken) ? wInitialIP : wIP_temp;
-if(wJumpTaken)
-{
-	wDestination=wBranchAddress;`
-}
-else if(wBranchTaken)
-{
-	if( wBranchAddress[5])
-	{
-	wDestination=wIP-{0,0,0,0,0,0,wBranchAddress[4],wBranchAddress[3],wBranchAddress[2],wBranchAddress[1],wBranchAddress[0]};
-	}
-	if( ~wBranchAddress[5])
-	{ wDestination=wIP+{0,0,0,0,0,0,wBranchAddress[4],wBranchAddress[3],wBranchAddress[2],wBranchAddress[1],wBranchAddress[0]}  ;}
-}
-UPCOUNTER_POSEDGE IP
-(
-.Clock(   Clock                ), 
-.Reset(   Reset | wBranchTaken ),
-.Initial( wInitialIP + 1  ),
-.Enable(  1'b1                 ),
-.Q(       wIP_temp             )
-);
-
-
-
-
-endmodule
-
-
-
-
-
-
-
 module UPCOUNTER_POSEDGE 
 (
 input wire Clock, Reset,
@@ -51,7 +5,6 @@ input wire [9:0] Initial,
 input wire Enable,
 output reg [ 9:0] Q
 );
-
 
   always @(posedge Clock )
   begin
@@ -65,3 +18,54 @@ output reg [ 9:0] Q
 		end			
   end
 endmodule
+
+
+module pc_Decider(
+input wire Clock,
+input wire Reset,
+output wire [9:0]wIP,
+input wire wBranchTaken,
+input wire wJumpTaken,
+input wire [9:0] wBranchAddress
+);
+
+wire [9:0]wInitialIP;
+wire [9:0]wDestination;
+assign wInitialIP = (Reset) ? 10'b0 : wDestination;
+assign wIP = (wBranchTaken|wJumpTaken) ? wInitialIP : wIP_temp;
+
+	always @(posedge Clock )
+	begin
+	if(wJumpTaken){
+		wDestination = wBranchAddress;
+	}
+	else if(wBranchTaken){
+		if( wBranchAddress[5]){
+			wDestination = wIP-{0,0,0,0,0,wBranchAddress[4],wBranchAddress[3],wBranchAddress[2],wBranchAddress[1],wBranchAddress[0]};
+			}
+		if( ~wBranchAddress[5]){ 
+			wDestination = wIP+{0,0,0,0,0,wBranchAddress[4],wBranchAddress[3],wBranchAddress[2],wBranchAddress[1],wBranchAddress[0]};
+			}
+	}
+	end
+
+
+
+UPCOUNTER_POSEDGE IP(
+	.Clock(Clock), 
+	.Reset(Reset | wBranchTaken ),
+	.Initial(wInitialIP + 1),
+	.Enable(1'b1),
+	.Q(wIP_temp)
+	);
+
+
+endmodule
+
+
+
+
+
+
+
+
