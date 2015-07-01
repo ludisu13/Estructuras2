f1d83b56a88665b5994370d58c58f37fd170eb16
Erick Eduarte <erickedur@gmail.com>
2015-07-01 00:35:38 -0600

    Generador Deco

--- Tarea_final_Decodificador/generador_decodificador.v
+++ Tarea_final_Decodificador/generador_decodificador.v
@@ -34,13 +34,13 @@ output reg [15:0] wInstruction;
 initial
 	begin
 	
-	#10 validdata = 8'd8; Readtoa = 0; Readtob = 0; iWriteEnable = 1; iAddress =10'd0; iReadAddress =10'd0;
-	#500 validdata = 8'd8; Readtoa = 1; Readtob = 0; iWriteEnable = 0; iAddress =10'd0;iReadAddress =10'd0; 
-	#1000 validdata = 8'd16; Readtoa = 0; Readtob = 0; iWriteEnable = 1; iAddress =10'd10; iReadAddress =10'd10;
-	#1500 validdata = 8'd16; Readtoa = 0; Readtob = 1; iWriteEnable = 0; iAddress =10'd10; iReadAddress =10'd10; 
-	#2000 validdata = 8'd32; Readtoa = 0; Readtob = 0; iWriteEnable = 1; iAddress =10'b0000011111;iReadAddress =10'b0000011111;
-	#2500 validdata = 8'd32;  Readtoa = 1; Readtob = 0; iWriteEnable = 0;iAddress =10'b0000011111;iReadAddress =10'b0000011111;
-	#3000 validdata = 8'd64;  Readtoa = 0; Readtob = 0; iWriteEnable = 1; iAddress =10'b1111111111;  iReadAddress =10'b1111111111;
-	#3500 validdata = 8'd64;  Readtoa = 0; Readtob = 1; iWriteEnable = 0; iAddress =10'b1111111111;  iReadAddress =10'b1111111111;
+	#10 validdata = 8'd8; wZa = 0; wZb = 0; wCa = 1; wCb = 0; wNa = 0; wNb = 0; wInstruction = 15'd0;
+	#500 validdata = 8'd8; wZa = 1; wZb = 0; wCa = 0; wCb = 0; wNa = 0; wNb = 0; wInstruction = 15'd0;
+	#1000 validdata = 8'd16; wZa = 0; wZb = 0; wCa = 1; wC = b0; wNa = 0; wNb = 0; wInstruction = 15'd0;
+	#1500 validdata = 8'd16; wZa = 0; wZb = 1; wCa = 0; wCb = 0; wNa = 0; wNb = 0; wInstruction = 15'd0;
+	#2000 validdata = 8'd32; wZa = 0; wZb = 0; wCa = 1; wCb = 1; wNa = 1; wNb = 0; wInstruction = 15'd0;
+	#2500 validdata = 8'd32;  wZa = 1; wZb = 0; wCa = 0; wCb = 1;wNa = 1; wNb = 0; wInstruction = 15'd0;
+	#3000 validdata = 8'd64;  wZa = 0; wZb = 0; wCa = 1; wCb = 1; wNa = 1; wNb = 0; wInstruction = 15'd0;
+	#3500 validdata = 8'd64;  wZa = 0; wZb = 1; wCa = 0; wCb = 1; wNa = 1; wNb = 0; wInstruction = 15'd0;
 	end
 endmodule
