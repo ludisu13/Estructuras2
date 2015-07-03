e459da809c32dc4a940c725f29bbac7e29223f18
Erick Eduarte <erickedur@gmail.com>
2015-07-02 23:06:07 -0600

    mediofunca

--- tarea45/Module_ROM.v
+++ tarea45/Module_ROM.v
@@ -1,11 +1,13 @@
-
+/*
+Esta es la rom que se carga de un archivo de texto
+*/
 
 module ROM
 (
 	input  wire[9:0]  		iAddress,
 	output reg [15:0] 		oInstruction
 );	
-reg [15:0] Memory [10:0];
+reg [15:0] Memory [9:0];
 initial $readmemh("test.txt",Memory);
 always @ ( iAddress )
 begin
